ab9cee46f8a13bf9b651d9dcd0b9a4fe36ccf4fd
tthuha236 <tthuha236@gmail.com>
2025-08-01 17:06:21 +0900

    Add config info for dags

---
+++ airflow/dags/configs/aws.py
@@ -0,0 +1,6 @@
+# aws info
+AWS_CONN_ID = "estat-airflow-aws-connection"
+S3_BUCKET = "estat-dbt-sf"
+RAW_FOLDER = "raw"
+STAGING_FOLDER = "stg"
+REGION_NAME = "ap-northeast-1"
---
+++ airflow/dags/configs/estat.py
@@ -0,0 +1,2 @@
+# data source info
+BASE_URL = "https://www.e-stat.go.jp"
