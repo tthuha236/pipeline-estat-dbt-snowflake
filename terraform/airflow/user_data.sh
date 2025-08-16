12346173882ed2cd9007c95f283a28756345e8d4
tthuha236 <tthuha236@gmail.com>
2025-08-16 21:08:43 +0900

    Fix airflow jenkinsfile

--- terraform/airflow/user_data.sh
+++ terraform/airflow/user_data.sh
@@ -9,18 +9,18 @@ sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # download git project
-sudo mkdir -p /opt/myproject
-cd /opt/myproject
-sudo git clone https://github.com/tthuha236/pipeline-estat-dbt-snowflake.git .
-cd airflow
+cd /opt
+sudo git clone https://github.com/tthuha236/pipeline-estat-dbt-snowflake.git 
+cd pipeline-estat-dbt-snowflake/airflow
 
 # copy airflow config file from s3 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-aws s3 cp s3://estat-dbt-sf/airflow-config/airflow.cfg /opt/myproject/airflow/config/
+sudo aws s3 cp s3://estat-dbt-sf/airflow-config/airflow.cfg ./config/
 
 sudo chmod +x /usr/lib/python3/dist-packages/cloudinit/config/cc_scripts_user.py
+sudo mkdir -p logs
 sudo chmod -R 777 logs/
 
 sudo docker-compose up
