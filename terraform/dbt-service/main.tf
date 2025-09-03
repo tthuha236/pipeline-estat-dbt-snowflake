3e81c71574952eb01ae88bd700e9b60cbe6f0b65
tthuha236 <tthuha236@gmail.com>
2025-09-04 06:53:10 +0900

    define iam role for ecs task

--- terraform/dbt-service/main.tf
+++ terraform/dbt-service/main.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_cluster" "dbt_service" {
 
 # define the secrets paramaters used in task
 data "aws_secretsmanager_secret" "dbt_profile_info" {
-    name = "estat/dbt/profile_info"
+    name = var.secrets_dbt_profile
 }
 
 data "aws_secretsmanager_secret_version" "dbt_profile_info_value" {
@@ -16,12 +16,73 @@ locals {
     dbt_profile_info_json = jsondecode(data.aws_secretsmanager_secret_version.dbt_profile_info_value.secret_string)
 }
 
+# create security group
+
+
+# create iam_role for task
+resource aws_iam_policy "access_secretsmanager_policy" {
+    name = "DbtSecretsManagerAccessPolicy"
+    policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Action": [
+            "secretsmanager:GetSecretValue",
+            "secretsmanager:DescribeSecret",
+            "secretsmanager:ListSecrets",
+			"secretsmanager:DescribeSecret",
+        ],
+        "Resource": "arn:aws:secretsmanager:${var.region}:${var.account_id}:secret:estat/dbt/*"
+        }
+    ]
+})
+}
+
+resource aws_iam_policy "get_ecr_authorization_policy" {
+    name = "DbtEcrAuthorizationPolicy"
+    policy = jsonencode({
+    "Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": [
+				"ecr:DescribeRegistry",
+				"ecr:DescribeImages",
+				"ecr:GetAuthorizationToken",
+				"ecr:DescribeRepositories"
+			],
+			"Resource": "*"
+		}
+	]
+    })
+}
+
+module "dbt_task_role" {
+    source = "../modules/iam_roles"
+    name = "dbt_task_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{
+        Effect = "Allow",
+        Principal = { Service = "ecs-tasks.amazonaws.com" },
+        Action = "sts:AssumeRole"
+        }]
+    })
+    policy_arns = [
+        aws.aws_iam_policy.access_secretsmanager_policy.arn,
+        aws.aws_iam_policy.get_ecr_authorization_policy.arn
+    ]
+}
+
 # create task definition
 resource "aws_ecs_task_definition" "service" {
   family = "dbt_task"
   requires_compatibilities = ["FARGATE"]
   cpu = 1024
   memory = 3072
+  task_role_arn = module.dbt_task_role.role_arn
   container_definitions = jsonencode([
     {
       "name"   : "dbt_container"
@@ -49,8 +110,4 @@ resource "aws_ecs_task_definition" "service" {
       ]
     }
   ])
-  
-
-
-
 }
--- terraform/dbt-service/variables.tf
+++ terraform/dbt-service/variables.tf
@@ -1,3 +1,4 @@
 variable cluster_name {}
 variable task_name {}
 variable container_image {}
+variable secrets_dbt_profile {}
