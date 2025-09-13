d1e716b2c0b25b3d47360b171f44499f6f577d5e
tthuha236 <tthuha236@gmail.com>
2025-09-13 22:03:16 +0900

    Add pipelines for dim location

---
+++ airflow/dags/dim_location_pipeline_dag.py
@@ -0,0 +1,68 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeSqlApiOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.exceptions import AirflowFailException
+from utils.load_config_info import load_config
+from docker.types import Mount
+from datetime import datetime
+import json
+import os
+
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeSqlApiOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.exceptions import AirflowFailException
+from utils.load_config_info import load_config
+from docker.types import Mount
+from datetime import datetime
+import json
+import os
+
+# # data source info
+# STAT_URL = "https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00200561&tstat=000000330001&cycle=1&tclass1=000000330001&tclass2=000000330004&tclass3=000000330005&tclass4val=0"
+dag_id = "dim_location_pipeline_dag"
+env = os.getenv("ENVIRONMENT", "")
+print(f"Running in ${env} environment")
+config = load_config(dag_id)
+
+# aws info
+aws_conn_id = config["aws"]["aws_conn_id"]
+region_name = config["aws"]["region_name"]
+
+with DAG(
+    dag_id = dag_id,
+    start_date = datetime(2025,7,28),
+    schedule = None,
+    catchup = False
+) as dag:
+# read sql file to load data from s3 bucket to stg table in snowflake
+    run_dbt_seed = EcsRunTaskOperator(
+        task_id = "run_dbt_seed",
+        cluster = config["aws"]["ecs"]["cluster"],
+        aws_conn_id = aws_conn_id,
+        task_definition = config["aws"]["ecs"]["task"],
+        launch_type="FARGATE",
+        overrides = {
+            "containerOverrides": [{
+                "name": config["dbt"]["image"],
+                "command": ["seed"]
+            }]
+        },
+        network_configuration= {
+             "awsvpcConfiguration": {
+             "subnets": config["aws"]["ecs"]["subnets"],
+             "securityGroups": config["aws"]["ecs"]["securityGroups"],
+             "assignPublicIp": "ENABLED",
+            }
+        },
+        region_name = region_name
+    )
+
+    run_dbt_seed
