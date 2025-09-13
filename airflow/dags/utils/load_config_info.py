f16b13fab2192eff79af874971b66d82bb5c5975
tthuha236 <tthuha236@gmail.com>
2025-09-13 22:20:19 +0900

    fix load_config file

--- airflow/dags/utils/load_config_info.py
+++ airflow/dags/utils/load_config_info.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 def deep_merge(dict1, dict2):
     """Recursively merge dict2 into dict1"""
     result = deepcopy(dict1)
+    if not dict2: return result
     for k, v in dict2.items():
         if k in result and isinstance(result[k], dict) and isinstance(v, dict):
             result[k] = deep_merge(result[k], v)
