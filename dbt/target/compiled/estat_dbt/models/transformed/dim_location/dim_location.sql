90b4e35be7b3554166a5dc1edbe5f36a189233b8
tthuha236 <tthuha236@gmail.com>
2025-09-20 22:09:45 +0900

    Add dbt model for dim_location

---
+++ dbt/target/compiled/estat_dbt/models/transformed/dim_location/dim_location.sql
@@ -0,0 +1,20 @@
+WITH city AS (
+    SELECT * FROM estat_db.estat_stg.dim_city_stg 
+),
+region AS (
+    SELECT * FROM estat_db.estat_stg.dim_city_region_stg
+)
+SELECT 
+    c.KEN_CODE,
+    c.SITYOUSON_CODE,
+    c.TIIKI_CODE,
+    c.KEN_NAME,
+    c.SITYOUSON_NAME1,
+    c.SITYOUSON_NAME2,
+    c.SITYOUSON_NAME3,
+    c.YOMIGANA,
+    r.CHIHOU,
+    CURRENT_TIMESTAMP() AS CREATED_TIME
+FROM city c
+JOIN region r
+  ON c.KEN_NAME = r.KEN_NAME
