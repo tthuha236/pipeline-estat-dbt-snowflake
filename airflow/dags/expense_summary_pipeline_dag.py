383fab21ca0acd657e6193592c58d311b1c616ea
tthuha236 <tthuha236@gmail.com>
2025-10-05 10:25:25 +0900

    Add dags for mart dbt models

--- airflow/dags/expense_summary_pipeline_dag.py
+++ airflow/dags/expense_summary_pipeline_dag.py
@@ -1,10 +1,14 @@
 from utils.load_config_info import load_config
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
 from datetime import datetime
 
 dag_id = "expense_summary_pipeline_dag"
 config = load_config(dag_id)
+models = config["dbt"]["models"]
+aws_conn_id = config["aws"]["aws_conn_id"]
+region_name = config["aws"]["region_name"]
 
 with DAG(
     dag_id=dag_id,
@@ -12,5 +16,24 @@ with DAG(
     schedule=None,
     catchup=False
 ) as dag:
-    dummy_task = EmptyOperator(task_id="dummy_task")
-    dummy_task
+    run_dbt = EcsRunTaskOperator(
+        task_id = "run_dbt_model",
+        cluster = config["aws"]["ecs"]["cluster"],
+        aws_conn_id = aws_conn_id,
+        task_definition = config["aws"]["ecs"]["task"],
+        launch_type="FARGATE",
+        overrides = {
+            "containerOverrides": [{
+                "name": config["dbt"]["image"],
+                "command": ["dbt","run", "--select", *models]
+            }]
+        },
+        network_configuration= {
+                "awsvpcConfiguration": {
+                "subnets": config["aws"]["ecs"]["subnets"],
+                "securityGroups": config["aws"]["ecs"]["securityGroups"],
+                "assignPublicIp": "ENABLED",
+            }
+        },
+        region_name = region_name
+)
--- streamlit/ec2_start_command.sh
+++ streamlit/ec2_start_command.sh
@@ -17,12 +17,23 @@ sudo apt install certbot python3-certbot-nginx -y
 # --------------------------------------------------------------
 echo "Configuring Nginx..." | tee -a $LOGFILE
 
+sudo bash -c 'cat <<EOF > /etc/nginx/conf.d/streamlit.conf
+# --- Limit request rate (per IP) ---
+limit_req_zone \$binary_remote_addr zone=one:10m rate=5r/m;
+
+# --- Limit concurrent connections (per IP) ---
+limit_conn_zone \$binary_remote_addr zone=addr:10m;
+EOF
+'
+
 sudo bash -c "cat <<'NGINX' > /etc/nginx/sites-available/streamlit
 server {
     listen 80;
     server_name 3.114.109.76;
 
     location / {
+        limit_req zone=one burst=10 nodelay;     # max 5 req/m, burst up to 10
+        limit_conn addr 2;                       # max 2 concurrent connections per IP
         proxy_pass http://127.0.0.1:8501;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
@@ -36,3 +47,5 @@ NGINX"
 sudo certbot --nginx -d kakei-dashboard.ddns.net
 sudo ln -sf /etc/nginx/sites-available/streamlit /etc/nginx/sites-enabled/
 sudo nginx -t && sudo systemctl restart nginx
+
+
